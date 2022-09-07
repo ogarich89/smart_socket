1380006719cb667ba6c368766af12f3c53b7cb88
ogarich89 <ogarich89@icloud.com>
2022-09-08 00:44:55 +0300

    Smart Socket GUI

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,33 +1,73 @@
-use std::io;
+use iced::widget::{Button, Column, Text};
+use iced::{button, Alignment, Element, Sandbox, Settings};
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str::from_utf8;
 
-fn main() {
-    match TcpStream::connect("localhost:3000") {
-        Ok(mut stream) => loop {
-            println!("Select option: \n\r 1. Get status \n\r 2. Toggle socket \n\r 3. Exit");
-            let mut buf = String::new();
-            io::stdin().read_line(&mut buf).unwrap();
-            let selected = buf.trim();
-            stream.write_all(selected.as_bytes()).unwrap();
+pub fn main() -> iced::Result {
+    SmartSocket::run(Settings::default())
+}
 
-            let mut data = [0_u8; 32];
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    if selected == "3" {
-                        break;
-                    }
-                    println!("{}", from_utf8(&data).unwrap().trim_matches(char::from(0)));
-                }
-                Err(e) => {
-                    println!("Failed to receive data: {}", e);
-                }
-            }
-        },
-        Err(e) => {
-            println!("Failed to connect: {}", e);
+struct SmartSocket {
+    status: String,
+    button: button::State,
+    stream: TcpStream,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Toggle,
+}
+
+fn send_command(command: i32, mut stream: &TcpStream) -> String {
+    stream.write_all(command.to_string().as_bytes()).unwrap();
+    let mut data = [0_u8; 32];
+    match stream.read_exact(&mut data) {
+        Ok(_) => from_utf8(&data)
+            .unwrap()
+            .trim_matches(char::from(0))
+            .to_string(),
+        Err(error) => {
+            panic!("Failed to receive data: {}", error);
         }
     }
-    println!("Goodbye!");
+}
+
+impl Sandbox for SmartSocket {
+    type Message = Message;
+
+    fn new() -> Self {
+        let stream = TcpStream::connect("localhost:3000").unwrap();
+        Self {
+            status: send_command(1, &stream),
+            button: Default::default(),
+            stream,
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("SmartSocket - Iced")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Toggle => {
+                let response = send_command(2, &self.stream);
+                println!("{}", response);
+                self.status = send_command(1, &self.stream);
+            }
+        }
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        let button = Button::new(&mut self.button, Text::new("Toggle")).on_press(Message::Toggle);
+        let text = Text::new(&self.status).size(50);
+
+        Column::new()
+            .push(text)
+            .push(button)
+            .padding(20)
+            .align_items(Alignment::Center)
+            .into()
+    }
 }
